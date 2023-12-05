eef3e2937b91f93d6ee4e92d9190d8eac569d947
Hengyu Liu <l372511387@163.com>
2023-12-05 20:10:28 +0000

    update: update sh file

--- Exp_FedSI_Cifar_large_addclient.sh
+++ Exp_FedSI_Cifar_large_addclient.sh
@@ -7,4 +7,5 @@ python main.py --dataset Cifar10 --datasize large --algorithm FedSOUL --num_glob
 python main.py --dataset Cifar10 --datasize large --algorithm BPFedPD --num_glob_iters 800 --batch_size 100 --zeta 1e-3 --add_new_client 100 --times 2 
 python main.py --dataset Cifar10 --datasize large --algorithm FedRep --num_glob_iters 800 --add_new_client 100 --times 2 
 python main.py --dataset Cifar10 --datasize large --algorithm LGFedAvg --num_glob_iters 800 --add_new_client 100 --times 2 
+python main.py --dataset Cifar10 --datasize small --algorithm FedSI --num_glob_iters 800 --learning_rate 0.01 --subnetwork_rate 0.07 --num_fineturn_iters 100 --add_new_client 100 --times 2 --optimizer Adam & 
 
